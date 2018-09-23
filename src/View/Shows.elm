6e5caa6cefb50df3cb079101546284d90460e230
omersagy <omersagyemails@gmail.com>
2018-09-23 15:03:09 -0400

    display show details.

--- src/View/Shows.elm
+++ src/View/Shows.elm
@@ -2,17 +2,27 @@ module View.Shows exposing (showsPage)
 
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (class)
+import Model exposing (Model, Show)
 import View.Asset as Asset
 
 
-showsPage : Html a
-showsPage =
+showsPage : Model -> Html a
+showsPage model =
     div [ class "shows-page" ]
         [ div [ class "subtitle" ] [ text "Upcoming" ]
-        , shows
+        , shows model
         ]
 
 
-shows : Html a
-shows =
-    text "some shows!"
+shows : Model -> Html a
+shows model =
+    div [] (List.map show model.shows)
+
+
+show : Show -> Html a
+show s =
+    div []
+        [ text s.name
+        , text s.date
+        , text s.boothName
+        ]
--- src/View/View.elm
+++ src/View/View.elm
@@ -39,7 +39,7 @@ body model =
             Mezuzot.mezuzotPage
 
         Shows ->
-            Shows.showsPage
+            Shows.showsPage model
 
         other ->
             div [] [ text "page not implemented!" ]
