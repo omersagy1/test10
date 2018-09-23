3f54cff6a839a20f82911f2976060cb58f547614
omersagy <omersagyemails@gmail.com>
2018-09-23 14:56:03 -0400

    add data file.

---
+++ src/Data.elm
@@ -0,0 +1,11 @@
+module Data exposing (shows)
+
+import Model exposing (Show)
+
+
+shows : List Show
+shows =
+    [ Show "Capitola Art & Wine Fair" "September 29th, 2018" "Booth #403"
+    , Show "Walnut Creek Art Walk" "October 2-3rd, 2018" "Booth #12"
+    , Show "Lafayette Art Expo" "October 12th, 2018" "Booth #19"
+    ]
